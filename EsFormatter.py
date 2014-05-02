d9e8c09d579e9710e8c62637c59d9ea02f8ca973
Leonardo Balter <leonardo.balter@gmail.com>
2014-05-02 10:20:57 -0300

    Remove debug line

--- EsFormatter.py
+++ EsFormatter.py
@@ -4,7 +4,6 @@ ON_WINDOWS = platform.system() is 'Windows'
 ST2 = sys.version_info < (3, 0)
 NODE = None
 
-print(os.environ['PATH'])
 if not ON_WINDOWS:
     # Extend Path to catch Node installed via HomeBrew
     os.environ['PATH'] += ':/usr/local/bin'
